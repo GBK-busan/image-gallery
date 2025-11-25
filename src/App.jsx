af7813ccb0ca7f37f1de2c36ab51122e68f04f30
GBK <lfejku17@gmail.com>
2025-11-25 10:33:03 +0900

    검색창 수정

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,9 @@ function App() {
   }, [term]);
   return (
     <div className="container mx-auto my-7">
-      <ImageSearch setTerm={setTerm} />
+      <div>
+        <ImageSearch setTerm={setTerm} />
+      </div>
       <div className="grid place-items-center sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-7">
         {images.map((image) => (
           <ImageCard key={image.id} image={image} />
